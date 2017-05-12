39dd7c4cca13e3445ebb07ebb88c6baf331f32b5
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-05-12 13:49:07 -0400

    https

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ var generateInternetQuote = function(){
 	var number = getRandomArbitrary(1, 99999);
 	
 	$.ajax({
-		url: "http://api.forismatic.com/api/1.0/?",
+		url: "https://api.forismatic.com/api/1.0/?",
 		dataType: "jsonp",
 		data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
 		success: function( response ) {
