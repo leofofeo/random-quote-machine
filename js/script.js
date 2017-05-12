17f68b699c4f6b476994645096ef4764ce3eec84
Leonardo Rubiano <leofofeo@Leonardos-iMac.lan>
2017-05-11 22:09:13 -0400

    wrapped up json file and most of the styling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,61 @@
-"//JS and jQuery for RQM" 
+//JS and jQuery for RQM
 $('document').ready(function(){
-	
-
+	$('#btn-internet').addClass('btn-primary');
 });
+
+
+////////// New Quote Functionality ////////////
+$('#btn-new-quote').on('click', function(){
+
+	var source = '';
+
+	if ($('#btn-internet').hasClass('btn-primary')){
+		source = 'internet';
+	} else {
+		source = 'office';
+	}
+
+	console.log(source);
+});
+
+////////// End New Quote Functionality ////////////
+
+
+////////// The Internet Functionality ////////////
+
+// Switch primary classes
+$('#btn-internet').on('click', function(){
+	if($('#btn-office').hasClass('btn-primary')){
+		$('#btn-office').removeClass('btn-primary');
+	}
+
+	if($('#btn-internet').hasClass('btn-primary')){
+	} else {
+		$('#btn-internet').addClass('btn-primary');
+	}
+});
+
+
+////////// End Internet Functionality ////////////
+
+
+////////// The Office Functionality ////////////
+
+// Switch primary classes
+$('#btn-office').on('click', function(){
+	if($('#btn-internet').hasClass('btn-primary')){
+		$('#btn-internet').removeClass('btn-primary');
+	}
+
+	if($('#btn-office').hasClass('btn-primary')){
+	} else {
+		$('#btn-office').addClass('btn-primary');
+	}
+});
+
+
+
+////////// End Office Functionality ////////////
+
+
+//Twitter Functionality
